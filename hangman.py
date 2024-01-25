a21443b4fc86ea24ccd3c3fb58dab2850d23eb28
Aviles17 <santiaviles17@gmail.com>
2024-01-25 11:56:27 -0500

    feature: Add option of file play, hangman game function, first version of the game

--- hangman.py
+++ hangman.py
@@ -37,6 +37,7 @@ def menu():
     print("2. Play Game with your own word file\n")
     print("3. Exit\n")
     choice = int(input("Enter your choice: "))
+    print("\n")
     return choice
 
 #Create function to clear console when started and finished game
@@ -62,7 +63,56 @@ def get_random_api_word(lenght_word: int):
         return None
 
 #Create function to get random word from file
+def get_random_file_word(f_path: str):
+    if os.path.exists(f_path):
+        with open(f_path, "r") as f:
+            words = f.readlines()
+            word = random.choice(words).strip()
+            return word
+    else:
+        print("File does not exist.\n")
+        raise FileNotFoundError
+    
+#Create game function
+def hangman_game(word: str):
+    print(word)
+    used_letters = []
+    word_completion = "_" * len(word)
+    for i in range(0, 6):
+        print(f"You have {6 - i} tries left.")
+        print (f"Used letters: {used_letters}")
+        print(f"Word: {word_completion}")
+        guess = input("Guess a letter or word: ").lower()
+        print("\n")
+        if len(list(guess)) > 1: #Check if the guess is a word
+            word_completion = check_letter(guess, word, word_completion) #Check if the letter is in the word and update the word_completion
+            used_letters.append(guess) #Update the used letters array
+            if "_" not in word_completion: #Check if the word is completed
+                clear_console()
+                print(f"You guessed the word {word} !")
+                print("YOU WIN!")
+                break
+        else:
+            if guess == word: #Check if the guess is the word
+                clear_console()
+                print(f"You guessed the word {word} !")
+                print("YOU WIN!")
+                break
+            
+    clear_console()
+    print(f"The word was {word} !")
+    print("YOU LOSE :(")
+        
 
+#Create function to check if letter is in word
+def check_letter(guess: str, word: str, word_completion: str):
+    char_list = list(word_completion)
+    if guess in word:
+        for i in range(len(word)):
+            if word[i] == guess:
+                char_list[i] = guess
+                
+    return ''.join(char_list)
 
 
 if __name__ == "__main__":
@@ -74,10 +124,14 @@ if __name__ == "__main__":
         if option == 1:
             input_lenght_word = int(input("Enter the lenght of the word: "))
             word = get_random_api_word(input_lenght_word)
-            print(word)
-        
+            clear_console()
+            hangman_game(word)
         elif option == 2:
-            pass
+            input_file_path = input("Enter the path of the file: ")
+            word = get_random_file_word(input_file_path)
+            #Add exception for path not found
+            clear_console()
+            hangman_game(word)
         elif option == 3:
             print("Thanks for playing. Goodbye!\n")
         else:

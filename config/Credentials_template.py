6b62a20d80a19d2a48505af79f44807b54c9269d
Aviles17 <santiaviles17@gmail.com>
2024-02-11 01:31:33 -0500

    feat: Create pilar function to request to OpenWeatherMap API with api key (Credentials.py)

---
+++ config/Credentials_template.py
@@ -0,0 +1,3 @@
+# This file is a template for the Credentials.py file. 
+# In order to use the API, you must create a Credentials.py file with the following information.
+API_KEY = ""
---
+++ scripts/api_managment_util.py
@@ -0,0 +1,17 @@
+import requests
+import json
+import logging
+from config.Credentials import API_KEY
+
+def get_weather_data(lat: float, lon: float, api_key= API_KEY) -> dict:
+    #Make an API call to OpenWeatherMap to retrieve the weather data for a specific location
+    if api_key != "":
+        url = f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&exclude=minutely,hourly&appid={api_key}"
+        response = requests.get(url)
+        logging.info(f"API call to OpenWeatherMap: {response.status_code}")
+        logging.info(f"API response: {response.url}")
+        if response is not None:
+            data = response.json()
+            return data
+        else:
+            raise Exception(f"API call failed with status code {response.status_code}")

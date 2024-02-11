589eb255a7b4ea1f8e6fbb18610b2f0f16818cd9
Aviles17 <santiaviles17@gmail.com>
2024-02-11 01:58:15 -0500

    feat: Setup basic GUI structure and second page implementation

--- scripts/api_managment_util.py
+++ scripts/api_managment_util.py
@@ -9,7 +9,7 @@ def get_weather_data(lat: float, lon: float, api_key= API_KEY) -> dict:
         url = f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&exclude=minutely,hourly&appid={api_key}"
         response = requests.get(url)
         logging.info(f"API call to OpenWeatherMap: {response.status_code}")
-        logging.info(f"API response: {response.url}")
+        logging.info(f"API response: {response}")
         if response is not None:
             data = response.json()
             return data
--- src/main.py
+++ src/main.py
@@ -13,6 +13,40 @@ Here are the steps you can take to create this project:
     Use the datetime library to display the current time and date. '''
     
 
-import tkinter
-import Pillow
-import datetime
+import tkinter as tk
+from PIL import Image #Pillow to display the weather icons (Images)
+import datetime as dt
+import logging
+from scripts.api_managment_util import get_weather_data
+
+def open_input_window():
+    #Create a new window to input the location
+    input_window = tk.Toplevel(main_window)
+    input_window.title("Location Input")
+    #Create labels and entry fields for the latitude and longitude
+    lbl_lat = tk.Label(input_window, text="Latitude:")
+    lbl_lat.pack(padx=10, pady=10)
+    entry_lat = tk.Entry(input_window)
+    entry_lat.pack(padx=10, pady=10)
+    lbl_lon = tk.Label(input_window, text="Longitude:")
+    lbl_lon.pack(padx=10, pady=10)
+    entry_lon = tk.Entry(input_window)
+    entry_lon.pack(padx=10, pady=10)
+    #Create a submit button to fetch the weather data
+    btn_submit = tk.Button(input_window, text="Submit", command=lambda: get_weather_data(float(entry_lat.get()), float(entry_lon.get())))
+    btn_submit.pack(padx=10, pady=10)
+    return btn_submit
+
+if __name__ == "__main__":
+    #Configure the logging module
+    logging.basicConfig(filename="weather_app.log", level=logging.DEBUG)
+    #Create a GUI for the weather app
+    main_window = tk.Tk()
+    main_window.title("Kreativstorm Weather App") #Create a title for the window
+    #btn_fetch = tk.Button(main_window, text="Fetch Location", command=fetch_location)
+    #btn_fetch.pack(padx=10, pady=10)
+
+    btn_input = tk.Button(main_window, text="Open Input Window", command=open_input_window)
+    btn_input.pack(padx=100, pady=100)    
+    main_window.mainloop()
+    
